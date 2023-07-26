dd3debce56aa514bb1307621a29f5377303db3b9
Jeremy Castle <x95castle1@yahoo.com>
2023-07-26 16:00:43 -0500

    Updated config for Cook App 2

--- src/main/java/cook/SecurityConfiguration.java
+++ src/main/java/cook/SecurityConfiguration.java
@@ -18,12 +18,10 @@ package cook;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@Profile("prod")
 public class SecurityConfiguration {
 
     @Bean
